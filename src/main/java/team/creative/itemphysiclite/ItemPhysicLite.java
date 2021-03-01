44f015ebe320abef875fa15d7c7e16e5230b53f4
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-03-01 17:03:31 +0100

    Formatted entire project

--- src/main/java/team/creative/itemphysiclite/ItemPhysicLite.java
+++ src/main/java/team/creative/itemphysiclite/ItemPhysicLite.java
@@ -41,178 +41,178 @@ import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(value = ItemPhysicLite.MODID)
 public class ItemPhysicLite {
-	
-	public static final Logger LOGGER = LogManager.getLogger(ItemPhysicLite.MODID);
-	
-	public static final String MODID = "itemphysiclite";
-	
-	public ItemPhysicLite() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-	}
-	
-	private static Minecraft mc = Minecraft.getInstance();
-	
-	private void doClientStuff(final FMLClientSetupEvent event) {
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-		MinecraftForge.EVENT_BUS.register(this);
-	}
-	
-	public static long lastTickTime;
-	
-	@SubscribeEvent
-	@OnlyIn(value = Dist.CLIENT)
-	public void renderTick(RenderTickEvent event) {
-		if (event.phase == Phase.END)
-			lastTickTime = System.nanoTime();
-	}
-	
-	public static boolean renderItem(ItemEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, net.minecraft.client.renderer.ItemRenderer itemRenderer, Random random) {
-		if (entityIn.getAge() == 0)
-			return false;
-		
-		matrixStackIn.push();
-		ItemStack itemstack = entityIn.getItem();
-		int i = itemstack.isEmpty() ? 187 : Item.getIdFromItem(itemstack.getItem()) + itemstack.getDamage();
-		random.setSeed(i);
-		IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(itemstack, entityIn.world, (LivingEntity) null);
-		boolean flag = ibakedmodel.isGui3d();
-		int j = getModelCount(itemstack);
-		
-		float rotateBy = (System.nanoTime() - lastTickTime) / 200000000F;
-		if (mc.isGamePaused())
-			rotateBy = 0;
-		
-		Vector3d motionMultiplier = getMotionMultiplier(entityIn);
-		if (motionMultiplier != null && motionMultiplier.lengthSquared() > 0)
-			rotateBy *= motionMultiplier.x * 0.2;
-		
-		matrixStackIn.rotate(Vector3f.XP.rotation((float) Math.PI / 2));
-		matrixStackIn.rotate(Vector3f.ZP.rotation(entityIn.rotationYaw));
-		
-		boolean applyEffects = entityIn.getAge() != 0 && (flag || mc.getRenderManager().options != null);
-		
-		//Handle Rotations
-		if (applyEffects) {
-			if (flag) {
-				if (!entityIn.isOnGround()) {
-					rotateBy *= 2;
-					Fluid fluid = getFluid(entityIn);
-					if (fluid == null)
-						fluid = getFluid(entityIn, true);
-					if (fluid != null)
-						rotateBy /= fluid.getAttributes().getDensity() / 1000 * 10;
-					
-					entityIn.rotationPitch += rotateBy;
-				}
-			} else if (entityIn != null && !Double.isNaN(entityIn.getPosX()) && !Double.isNaN(entityIn.getPosY()) && !Double.isNaN(entityIn.getPosZ()) && entityIn.world != null) {
-				if (entityIn.isOnGround()) {
-					if (!flag)
-						entityIn.rotationPitch = 0;
-				} else {
-					rotateBy *= 2;
-					Fluid fluid = getFluid(entityIn);
-					if (fluid != null)
-						rotateBy /= fluid.getAttributes().getDensity() / 1000 * 10;
-					
-					entityIn.rotationPitch += rotateBy;
-				}
-			}
-			
-			if (flag)
-				matrixStackIn.translate(0, -0.2, -0.08);
-			else if (entityIn.world.getBlockState(entityIn.getPosition()).getBlock() == Blocks.SNOW)
-				matrixStackIn.translate(0, 0.0, -0.14);
-			else
-				matrixStackIn.translate(0, 0, -0.04);
-			
-			double height = 0.2;
-			if (flag)
-				matrixStackIn.translate(0, height, 0);
-			matrixStackIn.rotate(Vector3f.YP.rotation(entityIn.rotationPitch));
-			if (flag)
-				matrixStackIn.translate(0, -height, 0);
-		}
-		
-		if (!flag) {
-			float f7 = -0.0F * (j - 1) * 0.5F;
-			float f8 = -0.0F * (j - 1) * 0.5F;
-			float f9 = -0.09375F * (j - 1) * 0.5F;
-			matrixStackIn.translate(f7, f8, f9);
-		}
-		
-		for (int k = 0; k < j; ++k) {
-			matrixStackIn.push();
-			if (k > 0) {
-				if (flag) {
-					float f11 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-					float f13 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-					float f10 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-					matrixStackIn.translate(f11, f13, f10);
-				}
-			}
-			
-			itemRenderer.renderItem(itemstack, ItemCameraTransforms.TransformType.GROUND, false, matrixStackIn, bufferIn, packedLightIn, OverlayTexture.NO_OVERLAY, ibakedmodel);
-			matrixStackIn.pop();
-			if (!flag)
-				matrixStackIn.translate(0.0, 0.0, 0.05375F);
-		}
-		
-		matrixStackIn.pop();
-		return true;
-	}
-	
-	public static Fluid getFluid(ItemEntity item) {
-		return getFluid(item, false);
-	}
-	
-	public static Fluid getFluid(ItemEntity item, boolean below) {
-		if (item.world == null)
-			return null;
-		
-		double d0 = item.getPosY();
-		BlockPos pos = item.getPosition();
-		if (below)
-			pos = pos.down();
-		
-		FluidState state = item.world.getFluidState(pos);
-		Fluid fluid = state.getFluid();
-		
-		if (fluid == null || fluid.getFluid().getAttributes().getDensity() == 0)
-			return null;
-		
-		if (below)
-			return fluid;
-		
-		double filled = state.getHeight();
-		
-		if (d0 - pos.getY() - 0.2 <= filled)
-			return fluid;
-		return null;
-	}
-	
-	public static int getModelCount(ItemStack stack) {
-		
-		if (stack.getCount() > 48)
-			return 5;
-		if (stack.getCount() > 32)
-			return 4;
-		if (stack.getCount() > 16)
-			return 3;
-		if (stack.getCount() > 1)
-			return 2;
-		
-		return 1;
-	}
-	
-	private static Field motionMultiplierField = null;
-	
-	public static Vector3d getMotionMultiplier(Entity entity) {
-		if (motionMultiplierField == null)
-			motionMultiplierField = ObfuscationReflectionHelper.findField(Entity.class, "field_213328_B");
-		try {
-			return (Vector3d) motionMultiplierField.get(entity);
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			return null;
-		}
-	}
+    
+    public static final Logger LOGGER = LogManager.getLogger(ItemPhysicLite.MODID);
+    
+    public static final String MODID = "itemphysiclite";
+    
+    public ItemPhysicLite() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    }
+    
+    private static Minecraft mc = Minecraft.getInstance();
+    
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    public static long lastTickTime;
+    
+    @SubscribeEvent
+    @OnlyIn(value = Dist.CLIENT)
+    public void renderTick(RenderTickEvent event) {
+        if (event.phase == Phase.END)
+            lastTickTime = System.nanoTime();
+    }
+    
+    public static boolean renderItem(ItemEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, net.minecraft.client.renderer.ItemRenderer itemRenderer, Random random) {
+        if (entityIn.getAge() == 0)
+            return false;
+        
+        matrixStackIn.push();
+        ItemStack itemstack = entityIn.getItem();
+        int i = itemstack.isEmpty() ? 187 : Item.getIdFromItem(itemstack.getItem()) + itemstack.getDamage();
+        random.setSeed(i);
+        IBakedModel ibakedmodel = itemRenderer.getItemModelWithOverrides(itemstack, entityIn.world, (LivingEntity) null);
+        boolean flag = ibakedmodel.isGui3d();
+        int j = getModelCount(itemstack);
+        
+        float rotateBy = (System.nanoTime() - lastTickTime) / 200000000F;
+        if (mc.isGamePaused())
+            rotateBy = 0;
+        
+        Vector3d motionMultiplier = getMotionMultiplier(entityIn);
+        if (motionMultiplier != null && motionMultiplier.lengthSquared() > 0)
+            rotateBy *= motionMultiplier.x * 0.2;
+        
+        matrixStackIn.rotate(Vector3f.XP.rotation((float) Math.PI / 2));
+        matrixStackIn.rotate(Vector3f.ZP.rotation(entityIn.rotationYaw));
+        
+        boolean applyEffects = entityIn.getAge() != 0 && (flag || mc.getRenderManager().options != null);
+        
+        //Handle Rotations
+        if (applyEffects) {
+            if (flag) {
+                if (!entityIn.isOnGround()) {
+                    rotateBy *= 2;
+                    Fluid fluid = getFluid(entityIn);
+                    if (fluid == null)
+                        fluid = getFluid(entityIn, true);
+                    if (fluid != null)
+                        rotateBy /= fluid.getAttributes().getDensity() / 1000 * 10;
+                    
+                    entityIn.rotationPitch += rotateBy;
+                }
+            } else if (entityIn != null && !Double.isNaN(entityIn.getPosX()) && !Double.isNaN(entityIn.getPosY()) && !Double.isNaN(entityIn.getPosZ()) && entityIn.world != null) {
+                if (entityIn.isOnGround()) {
+                    if (!flag)
+                        entityIn.rotationPitch = 0;
+                } else {
+                    rotateBy *= 2;
+                    Fluid fluid = getFluid(entityIn);
+                    if (fluid != null)
+                        rotateBy /= fluid.getAttributes().getDensity() / 1000 * 10;
+                    
+                    entityIn.rotationPitch += rotateBy;
+                }
+            }
+            
+            if (flag)
+                matrixStackIn.translate(0, -0.2, -0.08);
+            else if (entityIn.world.getBlockState(entityIn.getPosition()).getBlock() == Blocks.SNOW)
+                matrixStackIn.translate(0, 0.0, -0.14);
+            else
+                matrixStackIn.translate(0, 0, -0.04);
+            
+            double height = 0.2;
+            if (flag)
+                matrixStackIn.translate(0, height, 0);
+            matrixStackIn.rotate(Vector3f.YP.rotation(entityIn.rotationPitch));
+            if (flag)
+                matrixStackIn.translate(0, -height, 0);
+        }
+        
+        if (!flag) {
+            float f7 = -0.0F * (j - 1) * 0.5F;
+            float f8 = -0.0F * (j - 1) * 0.5F;
+            float f9 = -0.09375F * (j - 1) * 0.5F;
+            matrixStackIn.translate(f7, f8, f9);
+        }
+        
+        for (int k = 0; k < j; ++k) {
+            matrixStackIn.push();
+            if (k > 0) {
+                if (flag) {
+                    float f11 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
+                    float f13 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
+                    float f10 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
+                    matrixStackIn.translate(f11, f13, f10);
+                }
+            }
+            
+            itemRenderer.renderItem(itemstack, ItemCameraTransforms.TransformType.GROUND, false, matrixStackIn, bufferIn, packedLightIn, OverlayTexture.NO_OVERLAY, ibakedmodel);
+            matrixStackIn.pop();
+            if (!flag)
+                matrixStackIn.translate(0.0, 0.0, 0.05375F);
+        }
+        
+        matrixStackIn.pop();
+        return true;
+    }
+    
+    public static Fluid getFluid(ItemEntity item) {
+        return getFluid(item, false);
+    }
+    
+    public static Fluid getFluid(ItemEntity item, boolean below) {
+        if (item.world == null)
+            return null;
+        
+        double d0 = item.getPosY();
+        BlockPos pos = item.getPosition();
+        if (below)
+            pos = pos.down();
+        
+        FluidState state = item.world.getFluidState(pos);
+        Fluid fluid = state.getFluid();
+        
+        if (fluid == null || fluid.getFluid().getAttributes().getDensity() == 0)
+            return null;
+        
+        if (below)
+            return fluid;
+        
+        double filled = state.getHeight();
+        
+        if (d0 - pos.getY() - 0.2 <= filled)
+            return fluid;
+        return null;
+    }
+    
+    public static int getModelCount(ItemStack stack) {
+        
+        if (stack.getCount() > 48)
+            return 5;
+        if (stack.getCount() > 32)
+            return 4;
+        if (stack.getCount() > 16)
+            return 3;
+        if (stack.getCount() > 1)
+            return 2;
+        
+        return 1;
+    }
+    
+    private static Field motionMultiplierField = null;
+    
+    public static Vector3d getMotionMultiplier(Entity entity) {
+        if (motionMultiplierField == null)
+            motionMultiplierField = ObfuscationReflectionHelper.findField(Entity.class, "field_213328_B");
+        try {
+            return (Vector3d) motionMultiplierField.get(entity);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            return null;
+        }
+    }
 }
