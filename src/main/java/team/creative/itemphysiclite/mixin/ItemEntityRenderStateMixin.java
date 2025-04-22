e5487f529b09e5d6a479250dcfdf82dc6bbce5ef
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-04-22 09:38:37 +0200

    Fixed crash when rendering a chest

--- src/main/java/team/creative/itemphysiclite/mixin/ItemEntityRenderStateMixin.java
+++ src/main/java/team/creative/itemphysiclite/mixin/ItemEntityRenderStateMixin.java
@@ -46,8 +46,8 @@ public class ItemEntityRenderStateMixin implements ItemEntityRenderStateExtender
     @Override
     public void extractPhysic(ItemEntity entity) {
         ItemEntityRenderState state = (ItemEntityRenderState) (Object) this;
-        isBlock = state.item.usesBlockLight() && ((LayerRenderStateAccessor) ((ItemStackRenderStateAccessor) state.item).callFirstLayer()).getRenderType().getName().equals(
-            "item_entity_translucent_cull");
+        var renderType = ((LayerRenderStateAccessor) ((ItemStackRenderStateAccessor) state.item).callFirstLayer()).getRenderType();
+        isBlock = state.item.usesBlockLight() && (renderType == null || renderType.getName().equals("item_entity_translucent_cull"));
         ClientPhysic.calculateRotation(entity, state);
         additionalOffset = ItemPhysicLite.CONFIG.blockRequireOffset.is(entity.level().getBlockState(entity.blockPosition())) || ItemPhysicLite.CONFIG.blockBelowRequireOffset.is(
             entity.level().getBlockState(entity.blockPosition().below()));
